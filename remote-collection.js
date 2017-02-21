bfb7a8c17cd379fb6dc10d01a01dc370cd7d721c
Max <max.finke@gmail.com>
2017-02-21 08:43:48 +0000

    Allow passing process env var name for connectionOptions as OPLOG URL

--- remote-collection.js
+++ remote-collection.js
@@ -8,6 +8,10 @@ RemoteCollection = function(name, connectionString, connectionOptions) {
       connectionString = process.env[connectionString];
       if (!connectionString) throw new Error('Please either pass a MongoDB URI or name of an existing environment variable');
     }
+    if (typeof connectionOptions === 'string' || connectionOptions instanceof String) {
+      // Assume environment variable with OPLOG URL
+      connectionOptions = { oplogUrl: process.env[connectionOptions] };
+    }
     return new Mongo.Collection(name, {
       _driver: new MongoInternals.RemoteCollectionDriver(connectionString, connectionOptions)
     });
